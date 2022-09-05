59bfeeb0989fc5fb4c7278c3726c892a7750a937
Mona Lisa <stefanibusiness@gmail.com>
2022-09-05 14:00:49 -0700

    added get routes

--- server.js
+++ server.js
@@ -15,3 +15,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+  console.log(noteData);
+  res.json(noteData);
+
+  console.info(`${req.method} request received to get notes`);
+});
+
+
